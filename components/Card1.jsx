ec832476b17e8837ce993846e7cab6e5aa012616
farhangunadi <farhan19004@mail.unpad.ac.id>
2023-09-23 11:29:41 +0700

    add about us section

--- components/Card1.jsx
+++ components/Card1.jsx
@@ -3,14 +3,12 @@ import React from "react";
 
 export const Card1 = () => {
   return (
-    <div className=" h-3/5 w-auto overflow-hidden">
+    <div className="relative rounded-md h-[400px] w-[350px] max-w-xl p-3 border-2 mx-3">
       <Image
         src="/images/photo.jpg"
-        className=" object-cover w-full h-1/2"
         alt="services 1"
-        objectFit="cover"
-        width={300}
-        height={300}
+        layout="fill"
+        className="w-full h-auto object-cover"
       />
     </div>
   );
